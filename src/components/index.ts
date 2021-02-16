b56da75a6da55f441d8747ffdad0c7712fc5bc8c
trejgun <trejgun@gmail.com>
2021-02-16 19:57:34 +0800

    fix path

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export {IRichTextEditorProps, IRichTextEditorRef, RichTextEditor} from "./editor";
-export {IToolbarProps, Toolbar} from "./toolbar";
-export {TToolbarControl} from "./toolbar/types";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {IRichTextEditorProps, IRichTextEditorRef, RichTextEditor} from "./components/editor";
+export {IToolbarProps, Toolbar} from "./components/toolbar";
+export {TToolbarControl} from "./components/toolbar/types";
