c225e4c0a0652634d6620f397c4c341d4fea4031
trejgun <trejgun@gmail.com>
2021-02-17 19:14:31 +0800

    export utilities

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {IRichTextEditorProps, IRichTextEditorRef, RichTextEditor} from "./compon
 export {IToolbarProps, Toolbar} from "./components/toolbar";
 export {TToolbarControl, TCustomControl} from "./components/toolbar/types";
 export {TAnchor} from "./components/types";
+export {atomicBlockExists} from "./utils";
