8cfda992de49fcef6d1c5ced77bfb09895f48097
Karafizi Artur <artur@paktolus.com>
2021-05-20 09:49:30 +0800

    fix: fix for package.json and remove parameters from preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,13 +10,3 @@ export const decorators = [
     </ThemeProvider>
   ),
 ];
-
-export const parameters = {
-  actions: {argTypesRegex: "^on[A-Z].*"},
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-};
--- examples/inline-toolbar/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, {FC} from "react";
-
-import {RichTextEditor} from "../../src";
-
-export const InlineToolbar: FC = () => {
-  return <RichTextEditor label="Try selecting some text to show the inline toolbar..." inlineToolbar={true} />;
-};
