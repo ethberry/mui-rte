21c13cc2418edcb75217bf01b9e51d16a7da34dc
Karafizi Arthur <karafizi.artur@gmail.com>
2021-09-08 11:41:06 +0800

    feat: implement RichTextDisplay with customControls

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { IRichTextEditorProps, IRichTextEditorRef, RichTextEditor } from "./components/editor";
 export { IToolbarProps, Toolbar } from "./components/toolbar";
+export { IRichTextDisplayProps, RichTextDisplay } from "./components/display";
 export { TToolbarControl, TCustomControl } from "./components/toolbar/types";
 export { TAnchor } from "./components/types";
 export { atomicBlockExists } from "./utils";
