2c70dc626deb52d21b4ed2ef13f1c91584d34821
TrejGun <trejgun@gmail.com>
2022-04-04 19:45:10 +0800

    test: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,49 +1,30 @@
 module.exports = {
+  core: {
+    builder: "webpack5",
+  },
+  framework: "@storybook/react",
   stories: ["../storybook/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/preset-create-react-app"],
-  webpackFinal: config => {
-    config.module.rules.push({
-      test: /\.[tj]sx?$/,
-      exclude: [/node_modules/],
-      use: [
-        {
-          loader: "babel-loader",
-          options: {
-            babelrc: false,
-            presets: [
-              [
-                "@babel/env",
-                {
-                  modules: false,
-                  targets: {
-                    browsers: ["> 1%"],
-                  },
-                },
-              ],
-              [
-                "@babel/typescript",
-                {
-                  isTSX: true,
-                  allExtensions: true,
-                },
-              ],
-              [
-                "@babel/react",
-                {
-                  runtime: "automatic",
-                },
-              ],
-            ],
-            plugins: [
-              "optimize-clsx",
-              "@babel/plugin-proposal-nullish-coalescing-operator",
-              "@babel/plugin-proposal-optional-chaining",
-              "babel-plugin-inline-import",
-            ],
-          },
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/preset-create-react-app",
+      options: {
+        craOverrides: {
+          fileLoaderExcludes: ["less"],
         },
-      ],
-    });
+      },
+    },
+  ],
+  webpackFinal: config => {
+    const {
+      module: {
+        rules: [, , , , , { oneOf }],
+      },
+    } = config;
+    const babelLoader = oneOf.find(({ test }) => new RegExp(test).test(".ts"));
+    babelLoader.include = [/src/, /.storybook/];
+    babelLoader.options.sourceType = "unambiguous";
     return config;
   },
 };
